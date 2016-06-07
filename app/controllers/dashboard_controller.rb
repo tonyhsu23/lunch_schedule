7c7db10c60c60b8e3c70542a7834b8a0837d15cf
Tony Hsu <t153426789@gmail.com>
2016-06-07 22:19:06 +0800

    Create events' dashboard - retrieve only events that created by the people in the same company - fix many_to_many relations error between event and user through   event_people

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    Event.dashboard(current_user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   belongs_to :company
   belongs_to :department
   has_many :events, through: :event_people
+  has_many :event_people
+
+  def self.colleagues(company_id)
+    where(company_id: company_id)
+    .pluck(:id)
+  end
 end
