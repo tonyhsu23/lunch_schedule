24d149fb119b5abca9e9e7d840ba133753694656
Tony Hsu <t153426789@gmail.com>
2016-08-01 22:44:13 +0800

    Ajax create table row for restaurant and dish, lazy load dish category with dishes in restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,7 +6,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
-    @dishes = @restaurant.dishes
+    @dishes = @restaurant.dishes.includes(:dish_category)
     @dish = Dish.new
   end
 
