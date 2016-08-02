3aa16075a838373fd304c15a78aa0810ccbaea18
Tony Hsu <t153426789@gmail.com>
2016-08-02 17:31:20 +0800

    Add flash message partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when "notice" then "success"
+    when "error" then "danger"
+    when "alert" then "warning"
+    end
+  end
+
   def on_events_page?
     if params[:controller] == 'events'
       true
