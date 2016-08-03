9389f183775860a3af40e2f9e6eb86304096a2d6
Tony Hsu <t153426789@gmail.com>
2016-08-03 21:35:51 +0800

    Validates form datas

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,7 @@
 class Dish < ActiveRecord::Base
   belongs_to :dish_category
   belongs_to :restaurant
+
+  validates :name, :price, presence: true
+  validates :dish_category, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :dishes, dependent: :destroy
   belongs_to :restaurant_category
+
+  validates :name, :address, :phone_number, presence: true
+  validates :restaurant_category, presence: true
 end
