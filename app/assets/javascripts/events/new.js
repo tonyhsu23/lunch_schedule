bf946236eb0dd8ea4a726f8e2ad355b48edc35ac
Tony Hsu <t153426789@gmail.com>
2016-08-05 11:14:47 +0800

    Add new events form chekcker

--- app/assets/javascripts/events/new.js
+++ app/assets/javascripts/events/new.js
@@ -1,4 +1,12 @@
 $(document).on("ready page:load", function() {
+  $(".btn-primary").click( function(e) {
+    checked = $(".events-new .participants input[type=checkbox]:checked").length;
+    if(!checked) {
+      $(".require-participant-text").html("*it is required!");
+      e.preventDefault();
+    }
+  });
+
   $(".restaurant_select").on("change", function() {
     $.ajax({
       url: "/get_rest_dishes",
@@ -6,7 +14,20 @@ $(document).on("ready page:load", function() {
       dataType: "script",
       data: {
         restaurant_id: $(".restaurant_select option:selected").val()
+      },
+      success: function() {
+        atLeastOneChecked();
       }
     });
   });
 });
+
+function atLeastOneChecked() {
+  $(".btn-primary").click( function(e) {
+    checked = $(".events-new .dish-check-box input[type=checkbox]:checked").length;
+    if(!checked) {
+      $(".require-dish-text").html("*it is required!");
+      e.preventDefault();
+    }
+  });
+}
