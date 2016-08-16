6d5e52be5528acab191e59230e5291881f1f7033
Tony Hsu <t153426789@gmail.com>
2016-08-16 20:13:05 +0800

    Add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,9 @@ module ApplicationHelper
       false
     end
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=38"
+  end
 end
